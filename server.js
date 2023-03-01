8146c86b9f2856d7e22d53ac8c262d226a92a0a1
bennyp11 <ben.r.pollock@gmail.com>
2023-03-01 12:16:31 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3005;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+// Cache to store game IDs and nicknames
+const gameCache = {};
+
+app.options('/', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+  res.setHeader('Access-Control-Allow-Methods', 'POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.status(200).send();
+});
+
+app.post('/start', (req, res) => {
+  const gameId = req.body.gameId;
+  const nickName = req.body.nickName;
+  
+  // Add nickname to game ID in cache
+  if (!gameCache[gameId]) {
+    gameCache[gameId] = [];
+  }
+  gameCache[gameId].push(nickName);
+  
+  console.log(`Received nickName: ${nickName}`);
+  console.log(`Received game ID: ${gameId}`);
+  console.log(`Game cache: ${JSON.stringify(gameCache)}`);
+  
+  res.send({ message: 'Game ID received' });
+});
+
+app.post('/join', (req, res) => {
+  const gameId = req.body.gameId;
+  const nickName = req.body.nickName;
+  
+  // Check if game ID exists in cache
+  if (!gameCache[gameId]) {
+    console.log(`Invalid game code: ${gameId}`);
+    res.status(400).send({ message: 'Invalid game code' });
+  } else {
+    // Add nickname to game ID in cache
+    gameCache[gameId].push(nickName);
+    
+    console.log(`Received nickName: ${nickName}`);
+    console.log(`Received game ID: ${gameId}`);
+    console.log(`Game cache: ${JSON.stringify(gameCache)}`);
+    
+    res.send({ message: 'Nickname added' });
+  }
+});
+
+app.get('/game/:gameId', (req, res) => {
+  const gameId = req.params.gameId;
+  
+  // Retrieve nicknames for game ID from cache
+  const nickNames = gameCache[gameId] || [];
+  
+  console.log(`Retrieving nickNames for game ID ${gameId}`);
+  console.log(`Nicknames: ${JSON.stringify(nickNames)}`);
+  
+  res.send({ nickNames });
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
