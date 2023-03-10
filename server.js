1d148765b6747f079fc187462f6fe9a77cc5af68
bennyp11 <ben.r.pollock@gmail.com>
2023-03-09 17:48:05 -0800

    made data ingestion of cards into a more efficient algorithm

--- server.js
+++ server.js
@@ -25,19 +25,39 @@ app.options('/', (req, res) => {
 app.post('/start', (req, res) => {
   const gameId = req.body.gameId;
   const nickName = req.body.nickName;
-  
+  const cards = JSON.parse(fs.readFileSync('./cards.json', 'utf8'));
+
+  const replyAllCards = [];
+  const inboxCards = [];
+
+  for (let i = cards.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [cards[i], cards[j]] = [cards[j], cards[i]];
+  }
+
+  for (const card of cards) {
+    if (card.CardType === "ReplyAll") {
+      replyAllCards.push(card);
+    } else if (card.CardType === "Inbox") {
+      inboxCards.push(card);
+    }
+  }
+
   // Add nickname to game ID in cache
   if (!gameCache[gameId]) {
-    gameCache[gameId] = [];
+    gameCache[gameId] = { nickNames: [], replyAllCards: [], inboxCards: [] };
   }
-  gameCache[gameId].push(nickName);
-  
-  console.log(`Received nickName: ${nickName}`);
-  console.log(`Received game ID: ${gameId}`);
-  console.log(`Game cache: ${JSON.stringify(gameCache)}`);
-  
+
+  gameCache[gameId].nickNames.push(nickName);
+  gameCache[gameId].replyAllCards = replyAllCards;
+  gameCache[gameId].inboxCards = inboxCards;
+
+  console.log(gameCache[gameId].inboxCards);
+
   res.send({ message: 'Game ID received' });
 });
+
+
 
 app.post('/join', (req, res) => {
   const gameId = req.body.gameId;
@@ -57,7 +77,7 @@ app.post('/join', (req, res) => {
 
     wss.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
-        const nickNames = gameCache[gameId] || [];
+        const nickNames = gameCache[gameId].nickNames || [];
         client.send(JSON.stringify({ nickNames }));
       }
     });
@@ -72,7 +92,8 @@ app.get('/hitreplyall/:gameId/:nickName/initaldeal', (req, res) => {
   console.log('NICKNAME: ', nickName);
   
   // Retrieve nicknames for game ID from cache
-  let nickNames = gameCache[gameId] || [];
+  let nickNames = gameCache[gameId].nickNames || [];
+  //console.log('initialDeal ROUTE: ',gameCache[gameId]);
 
   // Add new nickname to nickNames array
   console.log(nickNames);
@@ -80,22 +101,9 @@ app.get('/hitreplyall/:gameId/:nickName/initaldeal', (req, res) => {
   console.log(`Retrieving nickNames for game ID ${gameId}`);
   console.log(`Nicknames: ${nickNames}`);
 
-  const cards = JSON.parse(fs.readFileSync('./cards.json', 'utf8'));
 
-  const replyAllCards = cards.filter(card => card.CardType === "ReplyAll");
-  const inboxCards = cards.filter(card => card.CardType === "Inbox");
-
-  // Fisher-Yates shuffle algorithm
-  for (let i = replyAllCards.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [replyAllCards[i], replyAllCards[j]] = [replyAllCards[j], replyAllCards[i]];
-  }
-
-  const dealtCards = replyAllCards.slice(0, 7); // select the first 7 cards of the shuffled array
-
-  // Update nicknames for game ID in cache
   
-  res.send({ nickNames, dealtCards });
+  res.send({ nickNames });
 });
 
 /*app.get('/hitreply/:gameId/:nickName/initaldeal', (req, res) => {
